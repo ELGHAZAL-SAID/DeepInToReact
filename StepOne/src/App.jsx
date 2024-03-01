9341d2bf784bedc132362f4f5fc29018834a7070
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-03-01 15:08:51 +0100

    API fetching

--- StepOne/src/App.jsx
+++ StepOne/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState("");
+
+  const GetAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      console.log(data.slip.advice);
+      return data.slip.advice;
+    } catch {
+      console.log("Error while fetching API");
+    }
+  };
+
+  const adviceHandler = async () => {
+    const newAdvice = await GetAdvice();
+    setAdvice(newAdvice);
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    adviceHandler();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="advice">{advice}</h1>
+      <button onClick={adviceHandler}>Get Advice</button>
+      <Message c={count} />
     </>
-  )
+  );
 }
 
-export default App
+const Message = (props) => {
+  return (
+    <p>
+      You have read <strong>{props.c}</strong> advice
+    </p>
+  );
+};
+
+export default App;
