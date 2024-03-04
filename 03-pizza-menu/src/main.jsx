3763aab2e8ac47c00c893f76ee71f74bfcb42c43
ELGHAZAL-SAID <SAID.ELGHAZAL@EDU.UIZ.AC.MA>
2024-03-04 17:05:24 +0000

    rebuilding react project from scrush

--- 03-pizza-menu/src/main.jsx
+++ 03-pizza-menu/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App(){
+    return <h1>Hello React!</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
