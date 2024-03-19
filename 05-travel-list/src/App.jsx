9725cf7984af1c4a2824329f8577f86fe139017d
EL GHAZAL SAID <said.elghazal@edu.uiz.ac.ma>
2024-03-19 11:23:30 +0000

    feat: Add Form component and create Form component with input fields and submit button

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Logo from "./Components/Logo";
+import Form from "./Components/Form";
 
 function App() {
   return (
     <>
       <Logo />
+      <Form />
     </>
   );
 }
---
+++ 05-travel-list/src/Components/Form.jsx
@@ -0,0 +1,16 @@
+function Form() {
+  return (
+    <>
+      <h1>Form</h1>
+      <form action="" method="post">
+        <label for="name">number</label>
+        <input type="number" name="ItemsNumber" id="" />
+        <label for="item">Item</label>
+        <input type="text" name="item" />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+export default Form;
